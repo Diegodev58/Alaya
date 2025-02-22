885ee0d4486414b4a042645d35893997e8732272
diegoborregales7 <125694995+diegoborregales7@users.noreply.github.com>
2025-02-22 16:11:19 -0400

    Nueva versión 1.1

---
+++ index.js
@@ -0,0 +1,21 @@
+function collares(){
+    let url = "Collares/index.html"
+    window.location.href = url
+}
+
+function anillos(){
+    let url = "Anillos/index.html"
+    window.location.href = url
+}
+
+function pulse(){
+    let url = "pulceras/index.html"
+    window.location.href = url
+    
+}
+
+function Argollas(){
+    let url = "#"
+    window.location.href = url
+}
+
